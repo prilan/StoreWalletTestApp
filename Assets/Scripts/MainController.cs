5ff6aa22035878d2c4ed98a8b8d473cf99208682
prilan <prilan@mail.ru>
2023-04-20 10:27:07 +0400

    Далее

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,21 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainController : MonoBehaviour
 {
     [SerializeField] Button coinsIncrementButton;
+    [SerializeField] Button coinsZeroButton;
+
+    [SerializeField] Button crystsalsIncrementButton;
+    [SerializeField] Button crystsalsZeroButton;
 
     private void Awake()
     {
         coinsIncrementButton.onClick.AddListener(OnCoinsIncrementnButtonClick);
+        coinsZeroButton.onClick.AddListener(OnCoinsZeroButtonClick);
     }
 
     private void OnCoinsIncrementnButtonClick()
     {
         
+    }
+
+    private void OnCoinsZeroButtonClick()
+    {
+
     }
 }
