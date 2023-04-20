87cc8228ed5df7fe079a4fb03fcaa57611b4ef60
prilan <prilan@mail.ru>
2023-04-20 19:20:05 +0400

    Сделал

---
+++ Assets/Scripts/CurrencyType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    public enum CurrencyType
+    {
+        Coins,
+        Crystals
+    }
+
+    public static class CurrencyTypeUtility
+    {
+        public static string GetCurrencyByCurrencyType(CurrencyType currencyType)
+        {
+            return currencyType.ToString();
+        }
+    }
+}
--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,32 +1,74 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-//using WalletUtility;
+using WalletUtility;
 
-public class MainController : MonoBehaviour
+namespace MainApp
 {
-    [SerializeField] Button coinsIncrementButton;
-    [SerializeField] Button coinsZeroButton;
-
-    [SerializeField] Button crystsalsIncrementButton;
-    [SerializeField] Button crystsalsZeroButton;
-
-    private void Awake()
+    public class MainController : MonoBehaviour
     {
-        coinsIncrementButton.onClick.AddListener(OnCoinsIncrementnButtonClick);
-        coinsZeroButton.onClick.AddListener(OnCoinsZeroButtonClick);
-    }
+        [SerializeField] TMP_Text coinsText;
+        [SerializeField] Button coinsIncrementButton;
+        [SerializeField] Button coinsZeroButton;
 
-    private void OnCoinsIncrementnButtonClick()
-    {
-        WalletUtility.Wallet.
-        //Wallet.
-    }
+        [SerializeField] TMP_Text crystsalsText;
+        [SerializeField] Button crystsalsIncrementButton;
+        [SerializeField] Button crystsalsZeroButton;
 
-    private void OnCoinsZeroButtonClick()
-    {
+        private void Awake()
+        {
+            coinsIncrementButton.onClick.AddListener(OnCoinsIncrementnButtonClick);
+            coinsZeroButton.onClick.AddListener(OnCoinsZeroButtonClick);
 
+            crystsalsIncrementButton.onClick.AddListener(OnCrystsalsIncrementnButtonClick);
+            crystsalsZeroButton.onClick.AddListener(OnCrystsalsZeroButtonClick);
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Wallet.Instance.CreateCurrency(CurrencyTypeUtility.GetCurrencyByCurrencyType(CurrencyType.Coins));
+            Wallet.Instance.CreateCurrency(CurrencyTypeUtility.GetCurrencyByCurrencyType(CurrencyType.Crystals));
+        }
+
+        private void OnCoinsIncrementnButtonClick()
+        {
+            ActionWithCurrency(CurrencyType.Coins, coinsText, Wallet.Instance.IncrementCurrency);
+        }
+
+        private void OnCoinsZeroButtonClick()
+        {
+            ActionWithCurrency(CurrencyType.Coins, coinsText, Wallet.Instance.ZeroCurrency);
+        }
+        
+        private void OnCrystsalsIncrementnButtonClick()
+        {
+            ActionWithCurrency(CurrencyType.Crystals, crystsalsText, Wallet.Instance.IncrementCurrency);
+        }
+
+        private void OnCrystsalsZeroButtonClick()
+        {
+            ActionWithCurrency(CurrencyType.Crystals, crystsalsText, Wallet.Instance.ZeroCurrency);
+        }
+
+        private void ActionWithCurrency(CurrencyType currencyType, TMP_Text currencyText, Action<string> action)
+        {
+            string currencyString = CurrencyTypeUtility.GetCurrencyByCurrencyType(currencyType);
+            action(currencyString);
+            Currency currency = Wallet.Instance.GetCurrency(currencyString);
+            if (currency != null)
+            {
+                UpdateText(currencyText, currency.CurrencyValue);
+            }
+        }
+
+        private void UpdateText(TMP_Text targetText, int value)
+        {
+            targetText.text = value.ToString();
+        }
     }
 }
